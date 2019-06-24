b99af2d91ff821d018b6c8fb06acd1c9d2d7326c
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-06-24 10:01:33 +0530

    add installer

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+func main() {
+	fmt.Printf("Prism Installer for %s (%s)\n\n", runtime.GOOS, runtime.GOARCH)
+
+	fmt.Println("Installing...")
+	if err := DownloadFile(); err != nil {
+		panic(err)
+	}
+	fmt.Println("Done.")
+}
+
+func DownloadFile() error {
+	var file string
+	url := "https://github.com/PrismLang/binaries/raw/master/prism-" + runtime.GOOS + "-" + runtime.GOARCH
+
+	switch runtime.GOOS {
+	case "windows":
+		url += ".exe"
+
+		installDir := os.Getenv("SYSTEMROOT")
+		file = filepath.Join(installDir, "prism.exe")
+
+	default:
+		installDir := "/usr/bin"
+		file = filepath.Join(installDir, "prism")
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	out, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
